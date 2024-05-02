c09eca55283f0e655058917309be4e97e5ec1d82
borawakevikrant <borawakevikrant@gmail.com>
2024-05-02 17:49:19 +0530

    Added routing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', (req, res) => {
-  res.redirect('/Login/login.html');
+  res.redirect('/index.html');
 });
 
 app.listen(process.env.PORT || 3000, () => {
